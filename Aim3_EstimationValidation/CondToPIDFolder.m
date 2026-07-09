fdba3a7176a3170e2ba4692520050abb45f957a5
zheng <zheng@TinaZ>
2026-07-09 14:44:15 +1000

    Script for sorting TrainingTesting_ParticipantSpecific outputs to organise by participant instead of by conditions

---
+++ Aim3_EstimationValidation/CondToPIDFolder.m
@@ -0,0 +1,24 @@
+%% Sort LME results from Condition-specific to participant-specific folders
+%  e.g. Cond_01 contained all estimated torques of all participants within
+%       condition 1. This script moves estimates torques of each
+%       participant to their respective folders so that S01_All has
+%       estimated torques from all 12 conditions
+
+myDir = "C:\Users\zheng\OneDrive\Desktop\ENGG7291 Data\LME models\Participant dependent";
+sourceFolders = dir(fullfile(myDir, "Cond_*"));
+destFolders = dir(fullfile(myDir, "S*ALL"));
+
+for i = 1:12
+    for j = 1:10
+        n1 = (j-1)*2 + 1; n2 = j*2;
+    
+        sourceFiles = dir(fullfile(myDir, sourceFolders(i).name, "Cond_*.mat"));
+        sourceFile1 = fullfile(myDir, sourceFolders(i).name, sourceFiles(n1).name);
+        sourceFile2 = fullfile(myDir, sourceFolders(i).name, sourceFiles(n2).name);
+    
+        destFolder = fullfile(myDir, destFolders(j).name);
+
+        copyfile(sourceFile1, destFolder);
+        copyfile(sourceFile2, destFolder);
+    end
+end
